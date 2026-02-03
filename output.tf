2b7497594dcc346a3364cdbd08344659630bbb0f
Matias Rocca <matias.rocca@nemogroup.net>
2026-02-03 15:47:17 -0300

    prueba 1 de todo el worfklow y luego deploy de infra

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
+##Para que la salida luego del apply muestre la ip publica a la que hay que conectarse para probar la app##
 output "ec2_public_ip" {
   description = "IP pública de la EC2 donde corre la app"
   value       = aws_instance.app.public_ip
